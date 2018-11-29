80bcbd103a2bdd018aaa86c6150fb67dd45ac04e
Steve Popoola <stevepop@gmail.com>
2018-11-29 23:25:35 +0000

    Added login buttons to the home page

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,13 @@
       <div class="intro">
           <h1 class="text-center text-grey-light text-5xl p-6">Website of Shenstone FC</h1>
           <h2 class="text-center text-white text-4xl p-6">Coming soon...</h2>
+          <div class="flex justify-center mt-6">
+            <div class="logins">
+               <button class="text-white text-3xl px-4 py-2 bg-pink-dark hover:bg-pink-darker rounded">Player Login</button>
+             <button class="text-white text-3xl px-4 py-2 bg-purple-dark hover:bg-purple-darker ml-6 rounded">Manager Login</button>
+            </div>
+           
+          </div>
       </div>
     </div>
   </section>
@@ -23,35 +30,3 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-
-
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
